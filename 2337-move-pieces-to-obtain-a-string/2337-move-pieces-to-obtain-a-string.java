c380e96c59c6860c6faaf429fdee64615eacd225
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2024-12-05 22:21:22 +0100

    Time: 18 ms (38.55%), Space: 45.7 MB (41.21%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        if(start == target) return true;
+        int left = 0;
+        int right = 0;
+
+        for(int i=0; i<start.length(); i++){
+            if(start.charAt(i) == 'R'){
+                right++;
+                if(left != 0) return false;
+            }
+            else if(start.charAt(i) == 'L'){
+                left--;
+            }
+            if(target.charAt(i) == 'R'){
+                right--;
+            }
+            else if(target.charAt(i) == 'L'){
+                left++;
+                if(right != 0) return false;
+            }
+            if(left < 0 || right < 0) return false;
+        }
+        
+        
+        return left == 0 && right == 0;
+    }
+}
