4f00a84c8cbe10e20b80fa73b183df3d09572c73
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2024-12-05 22:54:43 +0100

    Time: 2 ms (64.06%), Space: 41.4 MB (45.37%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index = 0;
+        if(s.length() == 0) return true;
+        for(int i=0; i < t.length(); i++){
+            if(s.charAt(index) == t.charAt(i)) index++;
+            if(index == s.length()) return true;
+        }
+        return (index == s.length());
+    }
+}
