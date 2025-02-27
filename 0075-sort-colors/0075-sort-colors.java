245546dc2e2d2638da0fe18c814e61a1230b5fa2
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2025-02-27 18:04:04 +0100

    Time: 0 ms (100%), Space: 41.9 MB (80.77%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,5 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums); // głupie i tak sie nie powinno XD, w wolnym czasie poprawić na jakiś lepszy algorytm
+    }
+}
