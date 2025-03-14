c39889c9bd6d0e092c40f4b3435472cea85bb3dd
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2025-03-14 19:22:32 +0100

    Time: 28 ms (60.08%), Space: 63.8 MB (7.93%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -20,28 +20,3 @@ class Solution {
         return (int) left;
     }
 }
-/*
- if(k < 1){
-        return 0;
-    }
-    long l = 1;
-    long r = 0;
-    for(int candie : candies){
-        r = Math.max(r,candie);
-    }
-    while(l <= r){
-        long mid = l + (r - l)/2;
-        long counter = 0;
-        for(int candie : candies){
-            counter += candie/mid;
-        }
-        if(counter >= k){
-            l = mid + 1;
-        }else{
-            r = mid - 1;
-        }
-    }
-    return (int)l - 1;
-
-    */
-    
