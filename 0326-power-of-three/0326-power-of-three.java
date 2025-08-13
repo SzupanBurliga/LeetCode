05ba7e99f3d66186b351ac5986d75837834a54a0
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2025-08-13 21:08:30 +0200

    Time: 8 ms (84.41%), Space: 44.8 MB (5.71%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        int num = 1;
-        
-        while(num <= Integer.MAX_VALUE/3){
-            if(num == n) return true;
-            num *= 3;
-        }
-        return num == n;
+        if(n == 1) return true;
+        if(n <= 0 || n % 3 != 0) return false;
+        return isPowerOfThree(n /3);
     }
 }
