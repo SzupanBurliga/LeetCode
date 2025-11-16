f8a1146edeba26a10783a59f22a116e10275d502
Szymon Burliga <148964630+SzupanBurliga@users.noreply.github.com>
2025-11-16 11:26:40 +0100

    Time: 2 ms (81.8%), Space: 47.8 MB (10.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index = 0;
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-                nums[index++] = nums[i];
+        //przypomnienie 2pointer
+        int left = 0;
+        for(int right = 0; right < nums.length; right++){
+            if(nums[right] != 0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
             }
-        }
-        for(int i=index; i<n; i++){
-            nums[i] = 0;
         }
     }
 }
